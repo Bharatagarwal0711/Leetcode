468ae42a9fd7714177d25079bde883aa7777d28d
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-05 14:10:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.99%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n == -1) return 1/x;
+
+        double ans = myPow(x,n/2);
+        if(n%2 == 0) return ans*ans;
+        else if(n%2 == 1) return ans*ans*x;
+        return ans*ans*(1/x);
+    }
+};
