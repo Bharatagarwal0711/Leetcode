c657b917da7023ae07048bc595ea459e0f46316b
Bharat <bharatagarwal0711@gmail.com>
2026-08-17 14:37:15 +0530

    Updating Repo Structure

---
+++ Matrix/36. Valid Sudoku/Solution.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        for(int i=0;i<9;i++){
+
+            vector<bool> row(10);
+            vector<bool> col(10);
+
+            for(int j=0;j<9;j++){
+                if(board[i][j] != '.'){
+                    if(row[board[i][j]-'0']) return false;
+                    row[board[i][j]-'0'] = 1;
+                }
+
+                if(board[j][i] != '.'){
+                    if(col[board[j][i]-'0']) return false;
+                    col[board[j][i]-'0'] = 1;
+                }
+
+            }
+        }
+
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                if(check(board,i,j)==false) return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    bool check(vector<vector<char>>& board,int row,int col){
+
+        unordered_set<char> temp;
+
+        for(int i=row;i<row+3;i++){
+            for(int j=col;j<col+3;j++){
+                if(board[i][j] != '.'){
+                    if(temp.find(board[i][j]) != temp.end() ) return false;
+                    temp.insert(board[i][j]);
+                }
+            }
+        }
+
+        return true;
+    }
+
+};
---
+++ Sets & Maps/380. Insert Delete GetRandom O(1)/Solution.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<random>
+using namespace std;
+
+class RandomizedSet {
+public:
+
+    unordered_map<int,int> mp;
+    vector<int> arr;
+
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(mp.find(val) != mp.end()) return false;
+
+        arr.push_back(val);
+        mp[val] = arr.size()-1;
+
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(mp.find(val) == mp.end()) return false;
+
+        int idx = mp[val];
+        int last = arr.back();
+
+        mp[last] = idx;
+        arr[idx] = last;
+
+        arr.pop_back();
+        mp.erase(val);
+
+        return true;
+    }
+    
+    int getRandom() {
+        return arr[rand() % arr.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
