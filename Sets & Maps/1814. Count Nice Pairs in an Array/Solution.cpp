20ba3b617e5523855fb4f9f051585734720aafc0
Bharat <bharatagarwal0711@gmail.com>
2026-08-17 18:25:21 +0530

    Updating Repo Structure

---
+++ Sets & Maps/1814. Count Nice Pairs in an Array/Solution.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int countNicePairs(vector<int>& nums) {
+        long long count = 0;
+
+        unordered_map<int,int> mp;
+
+        for(int ele: nums){
+
+            int key = ele-rev(ele);
+            count += mp[key];
+            mp[key]++ ;
+        }
+
+        return count % (1000000000+7);
+    }
+
+    int rev(int n){
+        int ans = 0;
+
+        while(n){
+            ans *= 10;
+            ans += n%10;
+            n /= 10;
+        }
+
+        return ans;
+    }
+};
