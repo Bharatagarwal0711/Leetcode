39a0bde6c6de143dc05952021b8c8a4d5ca086a2
Bharat <bharatagarwal0711@gmail.com>
2026-08-26 03:19:13 +0530

    Updating Repo Structure

---
+++ Sliding Window/1004. Max Consecutive Ones III/Solution.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0, j = 0;
+        int n = nums.size();
+        int ans = 0;
+
+        int no0 = 0;
+
+        while(j < n) {
+
+            if(nums[j] == 0)
+                no0++;
+
+            while(no0 > k) {
+                if(nums[i] == 0) no0--;
+                i++;
+            }
+
+            ans = max(ans, j - i + 1);
+            j++;
+        }
+
+        return ans;
+    }
+};
---
+++ Sliding Window/713. Subarray Product less than K/Solution.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k<=1) return 0;
+
+        int i=0, j=0;
+        int count = 0;
+        int product = 1;
+        int n = nums.size();
+
+        while(j<n){
+            product *= nums[j];
+
+            while(product>=k){
+                count += j-i;
+                product /= nums[i];
+                i++;
+            }
+            j++;
+        }
+        while(i<n){
+            count += j-i;
+            i++;
+        }
+
+        return count;
+
+    }
+};
