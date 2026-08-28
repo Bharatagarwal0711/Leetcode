e7e17d0d0a68640af2ca6eb2d57a8e39670a8413
Bharat <bharatagarwal0711@gmail.com>
2026-08-28 22:34:36 +0530

    28 August 2026

---
+++ Sets & Maps/705. Design HashSet/Solution.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class MyHashSet {
+public:
+    vector<bool> arr;
+    MyHashSet() {
+        arr.resize(1000001,false);
+    }
+    
+    void add(int key) {
+        arr[key] = 1;
+    }
+    
+    void remove(int key) {
+        arr[key] = 0;
+    }
+    
+    bool contains(int key) {
+        return arr[key];
+    }
+};
+
+/*
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
